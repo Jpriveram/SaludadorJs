cf3abca61cfb60ffe93738f8a84040ed4a8a8ffd
Juan Pablo Rivera Mercado <jpriveram01@gmail.com>
2024-02-17 18:36:14 -0400

    Se agrego la funcionalidad de saludo por horario

---
+++ src/fecha.js
@@ -0,0 +1,4 @@
+function s_fecha(fecha) {
+    return fecha; 
+}
+export default s_fecha;
--- src/presenter.js
+++ src/presenter.js
@@ -1,6 +1,8 @@
 import saludar from "./saludar";
 import s_edad from "./edad";
 import s_genero from "./genero";
+import s_hora from "./hora";
+import s_fecha from "./fecha";
 
 const nombre_input = document.querySelector("#nombre");
 const edad_input = document.querySelector("#edad");
@@ -14,24 +16,65 @@ form.addEventListener("submit", (event) => {
   const nombre = nombre_input.value;
   const edad = edad_input.value;
   const genero = genero_input.value;
+  let fechaActual = new Date();
+  let horaActual = fechaActual.getHours();
 
+if (horaActual >= 1 && horaActual < 12) {
   if(edad>=30 )
   {
     if (genero=='m') 
     {
-      div.innerHTML = "<p> Hola Señor " + saludar(nombre) + "</p>";
+      div.innerHTML = "<p> Buen Dia Señor " + saludar(nombre) + "</p>";
     }
     else
     {
-      div.innerHTML = "<p> Hola Señora " + saludar(nombre) + "</p>";
+      div.innerHTML = "<p> Buen Dia Señora " + saludar(nombre) + "</p>";
     }
    
   }
   else
   {
-    div.innerHTML = "<p> Hola " + saludar(nombre) + "</p>";
-
+    div.innerHTML = "<p> Buen Dia " + saludar(nombre) + "</p>";
   }
+}
+
+if (horaActual >= 12 && horaActual < 19) {
+  if(edad>=30 )
+  {
+    if (genero=='m') 
+    {
+      div.innerHTML = "<p> Buenas Tardes Señor " + saludar(nombre) + "</p>";
+    }
+    else
+    {
+      div.innerHTML = "<p> Buenas Tardes Señora " + saludar(nombre) + "</p>";
+    }
+   
+  }
+  else
+  {
+    div.innerHTML = "<p> Buenas Tardes " + saludar(nombre) + "</p>";
+  }
+}
+
+if (horaActual >= 19) {
+  if(edad>=30 )
+  {
+    if (genero=='m') 
+    {
+      div.innerHTML = "<p> Buenas Noches Señor " + saludar(nombre) + "</p>";
+    }
+    else
+    {
+      div.innerHTML = "<p> Buenas Tardes Noches " + saludar(nombre) + "</p>";
+    }
+   
+  }
+  else
+  {
+    div.innerHTML = "<p> Buenas Noches " + saludar(nombre) + "</p>";
+  }
+}
 
 
  // div.innerHTML = "<p> Hola " + saludar(nombre) + "</p>";
